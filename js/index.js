b6b4d3507b6f749b23f542c37a8f5656a2b487ef
Darren Whatford <darren.whatford@dpwit.co.uk>
2021-01-26 08:28:41 +0000

    Added the canvas confetti import to index.js

--- js/index.js
+++ js/index.js
@@ -2,3 +2,9 @@
 import { beachMountain } from './beach-mountain.js';
 
 beachMountain();
+
+import confetti from 'canvas-confetti';
+confetti.create(document.getElementById('canvas'), {
+  resize: true,
+  useWorker: true,
+ })({ particleCount: 200, spread: 200 });
